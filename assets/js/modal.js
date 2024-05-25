f9854d790822c97af339fc136e6fd624c977edb2
hannahschwen <hannahschwen@gmail.com>
2024-05-25 14:37:51 -0400

    adds pokemon to array when searched

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -16,3 +16,18 @@ fetch("https://pokeapi.co/api/v2/pokemon?limit=1302")
 // const modalbutton = document.getElementById('modalbutton');
 
 // modalbutton.addEventListener('click', getPokemon);
+
+const submitNewPokemon = document.getElementById('submit-new-pokemon');
+const pokemonInput = document.getElementById("pokelist")
+
+// add the pokemon input to array in local storage
+
+submitNewPokemon.addEventListener('click', function() {
+  let pokemonName = pokemonInput.value;
+  console.log(pokemonName);
+  if (pokemonName != "") {
+    pokemonRosterArray.push(pokemonName);
+    localStorage.setItem("pokemonRosterArray", pokemonRosterArray);
+    console.log(pokemonRosterArray);
+  }
+})
